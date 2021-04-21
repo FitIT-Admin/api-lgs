0d5164363bef9b4aedfaf43f433b02efe16bb05e
Carlos Castañeda <carlos.castaneda@axity.com>
2021-04-21 17:52:53 -0400

    Avances Formularios

--- src/controllers/form.controller.ts
+++ src/controllers/form.controller.ts
@@ -2,7 +2,6 @@ import {
   Count,
   CountSchema,
   Filter,
-  FilterExcludingWhere,
   repository,
   Where,
 } from '@loopback/repository';
@@ -11,21 +10,25 @@ import {
   param,
   get,
   getModelSchemaRef,
-  patch,
   put,
   del,
   requestBody,
 } from '@loopback/rest';
 import {Form} from '../models';
-import {FormRepository} from '../repositories';
+import {FormRepository, UserRepository} from '../repositories';
 import {SecurityBindings, securityId, UserProfile} from '@loopback/security';
 import {inject} from '@loopback/core';
 import {authenticate} from '@loopback/authentication';
 
+export class VigencyDate {
+  vigencyAt: Date;
+}
 export class FormController {
   constructor(
     @repository(FormRepository)
     public formRepository : FormRepository,
+    @repository(UserRepository)
+    public userRepository : UserRepository,
   ) {}
 
   @post('/forms', {
@@ -109,7 +112,10 @@ export class FormController {
   @authenticate('jwt')
   async findById(
     @param.path.string('slug') slug: string ): Promise<Form> {
-    return this.findSlugOrId(slug);
+      var form = await this.findSlugOrId(slug);
+      const user = await this.userRepository.find({ where : { rut : form.createdBy}});
+      form.createdBy = user[0].name + " " + user[0].lastName  + " " + user[0].secondLastName
+    return form;
   }
 
   @put('/forms/{slug}', {
@@ -131,6 +137,59 @@ export class FormController {
     formTemp.group = form.group;
     formTemp.ot = form.ot;
     formTemp.status = form.status;
+    await this.formRepository.updateById(formTemp.id, formTemp);
+  }
+
+  @put('/forms/approve/{slug}', {
+    responses: {
+      '204': {
+        description: 'Form PUT success',
+      },
+    },
+  })
+  @authenticate('jwt')
+  async approve(
+    @param.path.string('slug') slug: string,
+    @requestBody() form: VigencyDate,
+  ): Promise<void> {
+    const formTemp = await this.findSlugOrId(slug);
+    formTemp.publishAt = new Date();
+    formTemp.vigencyAt = form.vigencyAt;
+    formTemp.status = 1;
+    await this.formRepository.updateById(formTemp.id, formTemp);
+  }
+
+  @put('/forms/suspend/{slug}', {
+    responses: {
+      '204': {
+        description: 'Form PUT success',
+      },
+    },
+  })
+  @authenticate('jwt')
+  async suspend(
+    @param.path.string('slug') slug: string
+  ): Promise<void> {
+    const formTemp = await this.findSlugOrId(slug);
+    formTemp.suspendAt = new Date();
+    formTemp.status = 2;
+    await this.formRepository.updateById(formTemp.id, formTemp);
+  }
+
+  @put('/forms/delete/{slug}', {
+    responses: {
+      '204': {
+        description: 'Form PUT success',
+      },
+    },
+  })
+  @authenticate('jwt')
+  async delete(
+    @param.path.string('slug') slug: string
+  ): Promise<void> {
+    const formTemp = await this.findSlugOrId(slug);
+    formTemp.deleteAt = new Date();
+    formTemp.status = 3;
     await this.formRepository.updateById(formTemp.id, formTemp);
   }
 
