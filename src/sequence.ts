114b3e262cc22a357e4787c24535576a6ede4e81
Carlos Castañeda <carlos.castaneda@axity.com>
2021-06-16 22:30:12 -0400

    Correccion secuencia

--- src/sequence.ts
+++ src/sequence.ts
@@ -3,6 +3,7 @@ import {inject} from '@loopback/context';
 import {
   FindRoute,
   InvokeMethod,
+  InvokeMiddleware,
   ParseParams,
   Reject,
   RequestContext,
@@ -15,10 +16,12 @@ import * as Sentry from '@sentry/node';
 const SequenceActions = RestBindings.SequenceActions;
 
 export class MyAuthenticationSequence implements SequenceHandler {
+  @inject(SequenceActions.INVOKE_MIDDLEWARE, {optional: true})
+  protected invokeMiddleware: InvokeMiddleware = () => false;
+
   constructor(
     @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
-    @inject(SequenceActions.PARSE_PARAMS)
-    protected parseParams: ParseParams,
+    @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
     @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
     @inject(SequenceActions.SEND) protected send: Send,
     @inject(SequenceActions.REJECT) protected reject: Reject,
@@ -29,10 +32,9 @@ export class MyAuthenticationSequence implements SequenceHandler {
   async handle(context: RequestContext) {
     try {
       const {request, response} = context;
+      const finished = await this.invokeMiddleware(context);
+      if (finished) return;
       const route = this.findRoute(request);
-
-      await this.authenticateRequest(request);
-
       const args = await this.parseParams(request, route);
       const result = await this.invoke(route, args);
       this.send(response, result);
