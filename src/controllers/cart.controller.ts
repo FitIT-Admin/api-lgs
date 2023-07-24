28ee2fd81a8041c709c07a77766d7395023a1060
Ignacio Araya <chona.araya.h@gmail.com>
2023-07-24 19:14:34 -0400

    Nuevo modelo para Product, se elimina offers de Product y se agrega nuevo dato originalQty y se agregan nuevos endpoints para modificar el estado de las colecciones Order, Offer y Product.

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -24,8 +24,8 @@ import {authenticate} from '@loopback/authentication';
 import { ProductRepository } from '../repositories/product.repository';
 import { Product } from '../models/product.model';
 import { ObjectId } from 'mongodb';
-import { OrderRepository } from '../repositories';
-import { Order } from '../models';
+import { OfferRepository, OrderRepository } from '../repositories';
+import { Offer, Order } from '../models';
   //import {SecurityBindings, securityId, UserProfile} from '@loopback/security';
   //import {inject} from '@loopback/core';
   
@@ -35,9 +35,45 @@ import { Order } from '../models';
       //@repository(UserRepository) public userRepository: UserRepository,
       @repository(ProductRepository) public productRepository: ProductRepository,
       @repository(OrderRepository) public orderRepository: OrderRepository,
+      @repository(OfferRepository) public offerRepository: OfferRepository,
     ) {}
 
-    @get('/cart/products/{email}', {
+    @get('/cart/orders/{email}', {
+        responses: {
+          '200': {
+            description: 'Offer model instance',
+            content: {
+              'application/json': {
+                schema: getModelSchemaRef(Offer, {includeRelations: true}),
+              },
+            },
+          },
+        },
+      })
+      @authenticate('jwt')
+      async findOffersInCart(
+        @param.path.string('email') email: string
+      ): Promise<{ idOrder: string, offers: Offer[]}[]> {
+            try {
+                const orders: Order[] = await this.orderRepository.find({where: {status: 1, createBy: email}});
+                let orderOffers: { idOrder: string, offers: Offer[]}[] = [];
+                if (orders && orders.length > 0) {
+                  for (let order of orders) {
+                    const offers: Offer[] = await this.offerRepository.find({ where: {status: 2, idOrder: new ObjectId(order.id)} });
+                    orderOffers.push({
+                      idOrder: order.idOrder,
+                      offers: offers
+                    });
+                  }
+                  return (orderOffers && orderOffers.length > 0) ? orderOffers : []
+                }
+                return (orderOffers && orderOffers.length > 0) ? orderOffers : [];
+            } catch(error) {
+                console.log(error);
+                throw new HttpErrors.ExpectationFailed('Error al buscar por id');
+            }
+      }
+      @put('/cart/confirmed-payment/{id}', {
         responses: {
           '200': {
             description: 'Products model instance',
@@ -50,19 +86,34 @@ import { Order } from '../models';
         },
       })
       @authenticate('jwt')
-      async findProductsInCart(
-        @param.path.string('email') email: string
-      ): Promise<Product[]> {
+      async payOffers(
+        @param.path.string('id') id: string
+      ): Promise<void> {
             try {
-                const product: Product[] = await this.productRepository.find({where: {status: 3, createBy: email}});
-                console.log("/product/in-cart");
-                console.log(product);
-                return product;
+                const orders: Order[] = await this.orderRepository.find({where: { idOrder: id}});
+                if (orders && orders.length > 0) {
+                  const offers: Offer[] = await this.offerRepository.find({ where: {status: 2, idOrder: new ObjectId(orders[0].id)} });
+                  // IDs para cambiar de estado
+                  let offersId: string[] = [];
+                  for (let offer of offers) {
+                    offersId.push(offer.id);
+                  }
+
+                  // Definir la condición para seleccionar los registros a actualizar
+                  const filter = {
+                    idOffer: { $in: offersId },
+                  };
+
+                  // Definir el nuevo valor para el campo que se actualizará
+                  const update: {} = { status: 3 };
+
+                  console.log(await this.offerRepository.updateAll(update, filter));
+                }
             } catch(error) {
                 console.log(error);
                 throw new HttpErrors.ExpectationFailed('Error al buscar por id');
             }
-        }
+      }
     
   }
   
